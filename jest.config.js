7a0363ab88ab9102c822c2481cc214bafd8f08f0
admin <visualjupiter@gmail.com>
2018-07-12 11:55:03 +0900

    jest.config.jsを、公式github通りに変更

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "transform": {
         "^.+\\.tsx?$": "ts-jest"
     },
-    "testMatch": ['**/__tests__/**/*.test.(ts|js)?(x)'],
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
     "moduleFileExtensions": [
         "ts",
         "tsx",
